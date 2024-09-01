4efba96a62858ba843f304b2445b50384269fc03
Gianx64 <GianPol1337@gmail.com>
2024-08-31 22:27:10 -0400

    .env DB configs

--- api/mysql.js
+++ api/mysql.js
@@ -2,9 +2,11 @@ import mysql from 'mysql';
 
 const dbconfig = {
     host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || '3306',
     user: process.env.MYSQL_USER || 'root',
     password: process.env.MYSQL_PASSWORD || '',
     database: process.env.MYSQL_DB || 'FindMechs',
+    charset: process.env.MYSQL_CHARSET || 'utf8',
 }
 let connection;
 
