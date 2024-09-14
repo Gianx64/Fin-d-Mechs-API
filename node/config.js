0fe801d19503a3197c1530207eed8f590a8bb744
Gianx64 <GianPol1337@gmail.com>
2024-09-14 00:02:07 -0300

    config update

---
+++ node/config.js
@@ -0,0 +1,16 @@
+export default ({
+    app: {
+        port: 3000
+    },
+    jwt:{
+        secret: 'secretnotefortokengeneration'
+    },
+    pg: {
+        host: 'postgres',
+        port: '5432',
+        user: 'gian',
+        password: 'pass',
+        database: 'FindMechsDB',
+        charset: 'utf8'
+    }
+})
