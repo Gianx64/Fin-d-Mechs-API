d904487b1f6b4ac9cb28ab9612d6fd7539c0c5bb
Gianx64 <GianPol1337@Gmail.com>
2024-10-02 19:25:54 -0300

    Appointments update

--- node/api/controllers/appointments.js
+++ node/api/controllers/appointments.js
@@ -1,4 +1,5 @@
 import postgres from '../postgres.js';
+import authController from "../controllers/auth.js";
 
 const getAppointments = (req, res, next) => {
     try {
@@ -39,102 +40,111 @@ const getAppointment = (req, res, next) => {
 }
 
 const patchAppointment = (req, res, next) => {
-	switch (req.params.action) {
-		case '0':
-			//TODO: verify if confirmed or cancelled
-			try {
-				postgres.appointmentUpdate(req.body).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '1':
-			try {
-				postgres.appointmentCancel(req.body.id).then(() => {
-					res.status(200).json({
-						message: 'Cita cancelada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '2':
-			//TODO: verify if mech
-			try {
-				postgres.appointmentConfirm(req.body.id).then(() => {
-					res.status(200).json({
-						message: 'Cita confirmada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '3':
-			try {
-				postgres.appointmentCarTake(req.body.id).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '4':
-			try {
-				postgres.appointmentCarDeliver(req.body.id).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '5':
-			try {
-				postgres.appointmentComplete(req.body.id).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '6':
-			//TODO: verify if user
-			try {
-				postgres.appointmentCommentUser(req.body).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		case '7':
-			//TODO: verify if mech
-			try {
-				postgres.appointmentCommentMech(req.body).then(() => {
-					res.status(200).json({
-						message: 'Cita actualizada exitosamente.'
-					});
-				})
-			} catch(err) {
-				next(err);
-			}
-			break;
-		default:
-			break;
-	}
+    const user = authController.decodifyHeader(req);
+    switch (req.params.action) {
+        case '0':
+            if (req.body.confirmado || req.body.cancelado)
+                throw new Error('La cita ya no se puede modificar.');
+            try {
+                postgres.appointmentUpdate(req.body).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '1':
+            if (req.body.confirmado || req.body.cancelado)
+                throw new Error('La cita ya no se puede modificar.');
+            try {
+                postgres.appointmentCancel(req.body.id).then(() => {
+                    res.status(200).json({
+                        message: 'Cita cancelada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '2':
+            if (user.rol !== '10')
+                throw new Error('Solo el mech puede confirmar una cita.');
+            try {
+                postgres.appointmentConfirm(req.body.id).then(() => {
+                    res.status(200).json({
+                        message: 'Cita confirmada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '3':
+            try {
+                postgres.appointmentCarTake(req.body.id).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '4':
+            try {
+                postgres.appointmentCarDeliver(req.body.id).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '5':
+            try {
+                postgres.appointmentComplete(req.body.id).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '6':
+            //TODO: verify if user
+            if (user.rol !== '10')
+                throw new Error('Solo el usuario puede comentar como usuario.');
+            try {
+                postgres.appointmentCommentUser(req.body).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        case '7':
+            //TODO: verify if mech
+            if (user.rol !== '10')
+                throw new Error('Solo el mech puede comentar como mech.');
+            try {
+                postgres.appointmentCommentMech(req.body).then(() => {
+                    res.status(200).json({
+                        message: 'Cita actualizada exitosamente.'
+                    });
+                })
+            } catch(err) {
+                next(err);
+            }
+            break;
+        default:
+            break;
+    }
 }
 
 const cancelAppointment = (req, res, next) => {
--- node/api/routes/appointments.js
+++ node/api/routes/appointments.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import appointmentController from '../controllers/appointments.js';
+import authController from "../controllers/auth.js";
 
 const router = Router();
 
-router.get('/', appointmentController.getAppointments);
-router.post('/', appointmentController.postAppointment);
-router.get('/:appointmentId', appointmentController.getAppointment);
-router.patch('/:appointmentId/:action', appointmentController.patchAppointment);
-router.delete('/:appointmentId', appointmentController.cancelAppointment);
+router.get('/', authController.checkAuth, appointmentController.getAppointments);
+router.post('/', authController.checkAuth, appointmentController.postAppointment);
+router.get('/:appointmentId', authController.checkAuth, appointmentController.getAppointment);
+router.patch('/:appointmentId/:action', authController.checkAuth, appointmentController.patchAppointment);
+router.delete('/:appointmentId', authController.checkAuth, appointmentController.cancelAppointment);
 
 export default router;
