c0e3bbd9788b180409c7e0ddb2fedf27f3d4878c
Gianx64 <GianPol1337@gmail.com>
2024-10-08 12:35:55 -0300

    GET to POST method in auth route

--- node/api/routes/auth.js
+++ node/api/routes/auth.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', authController.getUserFromToken);
 router.post('/', authController.signUp);
-router.get('/signin', authController.signIn);
+router.post('/signin', authController.signIn);
 router.get('/:correo', authController.checkAuth, authController.readUser);
 
 export default router;
